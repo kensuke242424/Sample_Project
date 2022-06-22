0926a84eeaa1434410f47830c38dbf050fe371a2
kensuke242424 <kennsuke242424@gmail.com>
2022-06-22 23:55:09 +0900

    #20 メモアプリ 改良

--- MemoListSample/MemoListSample/Views/MemoListView.swift
+++ MemoListSample/MemoListSample/Views/MemoListView.swift
@@ -13,24 +13,33 @@ struct MemoListView: View {
     @State private var memos = [["にんじん", "10:01", "にんじんの美味しい料理の仕方を調べる。"],
                                 ["Swift勉強", "17:11", "SwiftUIでメモアプリ作成"],
                                 ["誕生日", "16:34", "プレゼントを買いに行く"]]
+    @State private var newMemo = ""
+    @State private var isEditting = false
 
     var body: some View {
 
         NavigationView {
-            List {
-                ForEach(memos, id: \.self) { memo in
+            VStack {
 
-                    NavigationLink(destination: MemoDetailView(memo: memo)) {
-                        MemoRowView(memo: memo)
+                TextField("予定を追加", text: $newMemo)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                List {
+                    ForEach(memos, id: \.self) { memo in
+
+                        NavigationLink(destination: MemoDetailView(memo: memo)) {
+                            MemoRowView(memo: memo)
+                        }
                     }
-                }
-                .onDelete(perform: delete)
-                .onMove(perform: move)
+                    .onDelete(perform: delete)
+                    .onMove(perform: move)
 
+                }
+                .navigationBarItems(trailing: EditButton())
+                .navigationTitle(Text("Memos"))
             }
-            .navigationBarItems(trailing: EditButton())
-            .navigationTitle(Text("Memos"))
-        }
+            }
+
+
     } // body
 
     // デリートメソッド
