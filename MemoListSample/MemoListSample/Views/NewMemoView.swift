8b6076483584037ed40af5456cdc71e8b3a7d999
kensuke242424 <kennsuke242424@gmail.com>
2022-06-25 17:49:54 +0900

    メモアプリ 更新

--- MemoListSample/MemoListSample/Views/NewMemoView.swift
+++ MemoListSample/MemoListSample/Views/NewMemoView.swift
@@ -11,12 +11,11 @@ struct NewMemoView: View {
 
     // フォーカスが当たるTextFieldを判断するためのenum
     enum Field: Hashable { case title }
-
+//
     @FocusState private var foucsedField: Field?
     @State var newMemoTitle = ""
     @State var newMemoText = ""
 
-    @State private var isEditting = false
     // Home画面に戻るためのプロパティ
     @Environment(\.presentationMode) private var presentationMode
     @EnvironmentObject var vm: MemoData
@@ -26,15 +25,15 @@ struct NewMemoView: View {
     var body: some View {
 
         VStack {
+            // メモのタイトルを記入するテキストボックス
             TextField("タイトル", text: $newMemoTitle)
                 .padding(20)
                 .frame(width: 400, height: 50)
                 .font(.title)
                 .focused($foucsedField, equals: .title)
 
-
+            // メモ詳細を記入するテキストボックス
             TextEditor(text: $newMemoText)
-            //                .ignoresSafeArea()
                 .padding(.horizontal)
 
         } // VStack
@@ -42,23 +41,23 @@ struct NewMemoView: View {
                             displayMode: .inline)
         .navigationBarItems(trailing: Button("完了") {
 
-            vm.memos[0]["memoTitle"] = newMemoTitle
-            vm.memos[0]["memoTime"] =
-            "\(Int.random(in: 0...1))\(Int.random(in: 0...9)) : \(Int.random(in: 0...5))\(Int.random(in: 0...9))"
-            vm.memos[0]["memoText"] = newMemoText
+//            vm.memos[0]["memoTitle"] = newMemoTitle
+//            vm.memos[0]["memoTime"] =
+//            "\(Int.random(in: 0...1))\(Int.random(in: 0...9)) : \(Int.random(in: 0...5))\(Int.random(in: 0...9))"
+//            vm.memos[0]["memoText"] = newMemoText
 
             self.presentationMode.wrappedValue.dismiss()
 
         }.padding())
 
-        // .onApperはView表示時に発生する処理のこと
-        // NewMemoViewへの遷移から、0.5秒ずらしてenumの値を更新し、キーボード表示の処理を行う
-        .onAppear() {
-
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
-                foucsedField = .title
-            }
-        }
+//         .onApperはView表示時に発生する処理のこと
+//         NewMemoViewへの遷移から、0.5秒ずらしてenumの値を更新し、キーボード表示の処理を行う
+//        .onAppear() {
+//
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
+//                foucsedField = .title
+//            }
+//        }
 
     } // body
 } // View
